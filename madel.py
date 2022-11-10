e0a11d8f0511eb6b4449084f39fd166922391e7d
elvisalibaba <117951682+elvisalibaba@users.noreply.github.com>
2022-11-10 23:37:28 +0100

    Create madel.py

---
+++ madel.py
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @elvisalibaba
+- 👀 I’m interested in python
+- 🌱 I’m currently learning python
+- 💞️ I’m looking to collaborate on devellloperment web
+- 📫 How to reach me coach
+
+<!---
+elvisalibaba/elvisalibaba is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
